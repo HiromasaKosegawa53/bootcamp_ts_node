faa02e5c110987fceb149b62651ddb5869074de9
= <=>
2023-08-09 17:57:09 +0900

    サバイバルTypeScript演習

---
+++ tsren/1-4.js
@@ -0,0 +1,5 @@
+function minus(x) {
+    return x - 1;
+}
+// console.log(minus(2));
+console.log(minus("two"));
---
+++ tsren/1-4.ts
@@ -0,0 +1,6 @@
+function minus(x: number) {
+  return x - 1;
+}
+// console.log(minus(2));
+console.log(minus("two"));
+
