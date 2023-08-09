25bce19e52dbea26007767e9f65405c51080a677
博将 <hiromasa@MacMini.local>
2023-08-09 10:00:35 +0900

    TypeScript lesson

---
+++ typescript_lessons/main.js
@@ -0,0 +1,21 @@
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+console.log("hello world");
+// JSの場合
+// var x = 10;
+// x = "hello";
+// TypeScript
+var i;
+var i = 10;
+// 型推論 i: number
+var i = 10;
+// any型 x: any
+var x;
+x = 10;
+x = "hello";
+// 配列
+var results;
+results = [10, 5, 3];
---
+++ typescript_lessons/main.ts
@@ -0,0 +1,25 @@
+class User {
+
+}
+console.log("hello world");
+
+// JSの場合
+// var x = 10;
+// x = "hello";
+
+// TypeScript
+var i: number;
+var i: number = 10;
+
+// 型推論 i: number
+var i = 10;
+
+// any型 x: any
+var x;
+x = 10;
+x = "hello";
+
+// 配列
+var results: number[];
+results = [10, 5, 3];
+
